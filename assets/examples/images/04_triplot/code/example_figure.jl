57b6237f55d61446c8d01e53c2034b19f14380cb
joa-quim <jluis@ualg.pt>
2022-06-16 00:24:05 +0000

    Deploying to gh-pages from  @ 5d3c7939145527b54dfed5f9dfbece02b9037cd5 🚀

--- assets/examples/images/04_triplot/code/example_figure.jl
+++ assets/examples/images/04_triplot/code/example_figure.jl
@@ -1,12 +1,12 @@
 # This file was generated, do not modify it. # hide
 begin # hide
     using GMT
-triplot(rand(5,3), lc=:red, title="Delaunay")
+triplot(rand(10,3), lc=:red, title="Delaunay")
 end # hide
 showfig(show=false)     # hide
 fname_ps = joinpath(tempdir(), "GMTjl_tmp.ps")  # hide
 gmt("psconvert -A2p -Qg4 -Qt4 " * fname_ps * " -TG *")  # hide
 GMT.current_cpt[1] = GMT.GMTcpt()   # hide
-mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_7381416103350412624.png"), force=true);    # hide
+mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_14516604640055573954.png"), force=true);    # hide
  
 nothing # hide
--- assets/examples/plotting_functions/50_plot3/code/example_figure.jl
+++ assets/examples/plotting_functions/50_plot3/code/example_figure.jl
@@ -1,13 +1,13 @@
 # This file was generated, do not modify it. # hide
 begin # hide
     using GMT
-D = gmtread("track_sample_data.txt", i="7,6,3");
+D = gmtread("../../../../../track_sample_data.txt", i="7,6,3");
 plot3d(D, lc=:gradient, lt=[1,5], par=(PS_LINE_CAP="round",))
 end # hide
 showfig(show=false)     # hide
 fname_ps = joinpath(tempdir(), "GMTjl_tmp.ps")  # hide
 gmt("psconvert -A2p -Qg4 -Qt4 " * fname_ps * " -TG *")  # hide
 GMT.current_cpt[1] = GMT.GMTcpt()   # hide
-mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_548430707481649220.png"), force=true);    # hide
+mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_12648245746102438778.png"), force=true);    # hide
  
 nothing # hide
