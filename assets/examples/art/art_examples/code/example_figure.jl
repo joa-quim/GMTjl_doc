e4505fe31bf9ba02123bf1057e6503a2b0329205
joa-quim <jluis@ualg.pt>
2022-06-28 01:08:54 +0000

    Deploying to gh-pages from  @ 3fd9586238118019806838892a65677f24082553 🚀

--- assets/examples/art/art_examples/code/example_figure.jl
+++ assets/examples/art/art_examples/code/example_figure.jl
@@ -3,18 +3,24 @@ begin # hide
 	using GMT   # hide
 	GMT.isFranklin[1] = true    # hide
 	getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
-	using GMT, GMT.Drawing
+	using GMT
 
-ellipse(300,201,0, 200, 50, units=:points, first=true, fill=:purple, pen=1);
-ellipse(340,206, 0,130, 66, fill=:purple, pen=1);
-ellipse(318,222,0, 60, 26, fill=:blue);
-box(200, 173, 205, 26, fill=:purple, pen=1);
-circle(305,185,56, fill=:black);
-circle(305,185,36, fill=:gray50);
-circle(400,185,56, fill=:black);
-circle(400,185,36, fill=:gray50, fmt=:png, show=true);
+t = GMT.linspace(0,2pi,360);
+x = cos.(4*t) .* cos.(t);
+y = cos.(4*t) .* sin.(t);
+
+lines([-0.7 -0.25 0], [-1.5 -0.8 0], # The flower stem
+      limits=(-1,1,-1.5,1),          # Fig limits
+      lw=9,                          # Stem's line width in points
+      lc=:darkgreen,                 # Stem's line color
+      bezier=true,                   # Smooth the stem polyne as a Bezier curve
+      figsize=(14,0),                # Fig size. Second arg = 0 means compute the height keeping aspect ratio
+      frame=:none)                   # Do not plot the frame
+plot!(x, y,
+      fill=(pattern=getpath4docs("tiling2.jpg"),  # Fill pattern file
+      dpi=200), show=true)           # The pattern DPI
 end # hide
-mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_12239339531825922968.png"), force=true);    # hide
+mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_14772517729074949607.png"), force=true);    # hide
 GMT.isFranklin[1] = false    # hide
 GMT.IamModern[1]  = false    # hide
  
