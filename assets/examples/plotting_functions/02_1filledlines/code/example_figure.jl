7f8738ff8f0eb8f0991a98dba6bc1fe2df701207
Joaquim Luis <jmfluis@gmail.com>
2022-12-04 18:39:54 +0000

    Commit

---
+++ assets/examples/plotting_functions/02_1filledlines/code/example_figure.jl
@@ -0,0 +1,13 @@
+# This file was generated, do not modify it. # hide
+begin # hide
+	using GMT   # hide
+	GMT.isFranklin[1] = true    # hide
+	getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
+	using GMT
+plot(sin, x->sin(2x), [0 2pi], fill="blue@70", pen=(1,"blue@40"), show=1)
+end # hide
+mv(joinpath(tempdir(), "GMTjl_tmp.png"), joinpath(@OUTPUT, "example_16255661678775668923.png"), force=true);    # hide
+GMT.isFranklin[1] = false    # hide
+GMT.IamModern[1]  = false    # hide
+ 
+nothing # hide
