2af7d20a1645e8bf3a905789f412ca48c88a6c46
joa-quim <jluis@ualg.pt>
2023-03-24 13:27:06 +0000

    Deploying to gh-pages from @ joa-quim/GMTjl_doc@ccd4389a61e16ec223c63963744481a36d37dd01 🚀

--- assets/documentation/modules/grd2cpt/code/example_figure.jl
+++ assets/documentation/modules/grd2cpt/code/example_figure.jl
@@ -1,6 +1,7 @@
 # This file was generated, do not modify it. # hide
 begin # hide
 	using GMT   # hide
+	GMT.gmt_restart()	# hide
 	GMT.isFranklin[1] = true    # hide
 	getpath4docs(file::String) = joinpath("..", "..", "..", "..", "..", file) # hide
 	using GMT
